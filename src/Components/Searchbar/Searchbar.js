972e5c214f3c77773c9113becdc6dfdcfbcc49ca
SergeySidorchuk <serg_ss@ukr.net>
2022-01-06 12:36:07 +0200

    added h1

--- src/Components/Searchbar/Searchbar.js
+++ src/Components/Searchbar/Searchbar.js
@@ -7,26 +7,27 @@ class Searchbar extends Component {
     };
 
     handleChange = (event) => {
-        this.setState({searchQuery: event.currentTarget.value})       
+        this.setState({ searchQuery: event.currentTarget.value })
     }
 
     handleSubmit = e => {
-        e.preventDefault();  
+        e.preventDefault();
         if (this.state.searchQuery.length !== 0) {
-            this.props.onChangeQuery(this.state.searchQuery)     
-        }  
+            this.props.onChangeQuery(this.state.searchQuery)
+        }
     }
 
     render() {
         return (
             <header className="Searchbar">
+                <h1 className="SearchTitle">Image finder</h1>
                 <form className="SearchForm" onSubmit={this.handleSubmit}>
                     <button type="submit" className="SearchForm-button">
                         <span className="SearchForm-button-label"> Search
                         </span>
                     </button>
 
-                    <input className="SearchForm-input" type="text" autoComplete="off" autoFocus placeholder="Search images and photos" value={this.state.searchQuery} onChange={this.handleChange}/>
+                    <input className="SearchForm-input" type="text" autoComplete="off" autoFocus placeholder="Search images and photos" value={this.state.searchQuery} onChange={this.handleChange} />
                 </form>
             </header>
         )
@@ -34,6 +35,6 @@ class Searchbar extends Component {
 }
 Searchbar.propTypes = {
     onSubmit: PropTypes.func.isRequired,
-  };
+};
 
 export default Searchbar;
